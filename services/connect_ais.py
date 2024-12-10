64d0e11c5f198f4cffc0d80f4f32dc2f6f9478fc
Mile Ristovski <mristovski@myges.fr>
2024-12-10 22:23:26 +0100

    Petit changements + tests pour le script python

--- services/connect_ais.py
+++ services/connect_ais.py
@@ -2,6 +2,8 @@ import asyncio
 import websockets
 from kafka import KafkaProducer
 import json
+import csv
+import os
 
 # Kafka configuration
 KAFKA_SERVER = 'localhost:9092'
@@ -13,13 +15,16 @@ producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 # AIS Stream API details
 AIS_STREAM_URL = 'wss://stream.aisstream.io/v0/stream'
 API_KEY = 'e66dea5edc96977c45bd4ddba1e1b34acf718a04'
-BOUNDING_BOXES = [[[25.835302, -80.207729], [25.602700, -79.879297]]]
+BOUNDING_BOXES = [[[-90, -180], [90, 180]]]
 
 # Subscription message
 subscription_message = json.dumps({
     "APIKey": API_KEY,
     "BoundingBoxes": BOUNDING_BOXES
 })
+data_folder = os.path.join("data")
+os.makedirs(data_folder, exist_ok=True)
+
 
 async def connect_to_ais_stream():
     async with websockets.connect(AIS_STREAM_URL) as websocket:
@@ -29,16 +34,24 @@ async def connect_to_ais_stream():
         async for message in websocket:
             decoded_message = json.loads(message.decode("utf-8"))
             
-            # Extract key and merge "Message" content with the rest
             key = decoded_message["MessageType"]
-            message_data = decoded_message.pop("Message")
-            message_data.update(decoded_message)
-            message_data.pop("MessageType")
-            # Serialize the modified data back to JSON
-            message_json = json.dumps(message_data)
+            message_json = json.dumps(decoded_message)
             
             # Send the processed message to Kafka
             producer.send(KAFKA_TOPIC, value=message_json.encode("utf-8"), key=key.encode("utf-8"))
+
+                        # Write the message to a CSV file
+            # csv_filename = os.path.join(data_folder, f"{key}.csv")
+            # csv_file_exists = os.path.exists(csv_filename)
+            
+            # with open(csv_filename, mode='a', newline='', encoding='utf-8') as csv_file:
+            #     csv_writer = csv.DictWriter(csv_file, fieldnames=decoded_message.keys())
+                
+            #     # Write the header only if the file is new
+            #     if not csv_file_exists:
+            #         csv_writer.writeheader()
+                
+            #     csv_writer.writerow(decoded_message)
 
 # Run the WebSocket client
 asyncio.get_event_loop().run_until_complete(connect_to_ais_stream())
